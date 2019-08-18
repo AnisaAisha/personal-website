ded17a92edc283f335254d927e8de92e62dce29e
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2019-08-19 00:39:31 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 $(document).ready(function () {
-    $('#about').fadeIn(1000).removeClass('hidden');
-	$("#nav-experience").click(function(){
-		$('#experience').fadeIn(1000).removeClass('hidden');
-	});
-	$("#nav-education").click(function(){
-		$('#education').fadeIn(1000).removeClass('hidden');
-	});
-	$("#nav-achievements").click(function(){
-		$('#achievements').fadeIn(1000).removeClass('hidden');
-	});
-	$("#nav-gallery").click(function(){
-		$('#gallery').fadeIn(1000).removeClass('hidden');
-	});
-	$("#nav-contact").click(function(){
-		$('#contact').fadeIn(1000).removeClass('hidden');
-	});
+	
+    $('.hidden').fadeIn(1000).removeClass('hidden');
+	
 });
+
 
